e1ec45b3e49e248ad028d05af29ace765f0fcca1
Dustin Broderick <dbprodigious@gmail.com>
2017-08-25 15:40:15 -0600

    Uses local 3rd party source because they are already there.

--- cmake/3rdparty.cmake
+++ cmake/3rdparty.cmake
@@ -9,12 +9,18 @@ set(THIRD_PARTY_INSTALL_DIR ${CMAKE_BINARY_DIR}/3rdparty)
 set(XERCES_REPO
     http://svn.apache.org/repos/asf/xerces/c/tags/Xerces-C_2_8_0)
 set(XERCES_URL
-    http://archive.apache.org/dist/xerces/c/2/sources/xerces-c-src_2_8_0.tar.gz)
-# set(XERCES_REPO https://svn.apache.org/repos/asf/xerces/c/trunk)
+    ${CMAKE_SOURCE_DIR}/build/3rdparty-dists/xerces-c-src_2_8_0.tar.bz2)
+    #http://archive.apache.org/dist/xerces/c/2/sources/xerces-c-src_2_8_0.tar.gz)
 set(XERCES_DIR ${CMAKE_CURRENT_BINARY_DIR}/xerces)
 set(XERCES_INSTALL ${THIRD_PARTY_INSTALL_DIR}/xerces)
 set(XERCES_ROOT ${XERCES_DIR}/source)
 set(XERCES_SRC ${XERCES_ROOT}/src/xercesc)
+
+IF(APPLE)
+    set(XERCES_PLATFORM macosx)
+ELSE(APPLE)
+    set(XERCES_PLATFORM linux)
+ENDIF(APPLE)
 
 ExternalProject_Add(xerces
     URL ${XERCES_URL}
@@ -27,7 +33,7 @@ ExternalProject_Add(xerces
         # Build steps lumped together because XercesC environment variables
         # Using bash script prevents rebuilding if .installed file exists
         # I dislike this, but...
-        sh ${CMAKE_SOURCE_DIR}/cmake/build_xerces.sh ${XERCES_SRC} ${XERCES_ROOT} ${XERCES_INSTALL}
+        sh ${CMAKE_SOURCE_DIR}/cmake/build_xerces.sh ${XERCES_SRC} ${XERCES_ROOT} ${XERCES_INSTALL} ${XERCES_PLATFORM}
 )
 set(XERCES_INCLUDE ${XERCES_INSTALL}/include)
 set(XERCES_LIB_DIR ${XERCES_INSTALL}/lib)
@@ -35,18 +41,20 @@ set(XERCES_LIBRARIES -lxerces-c -lxerces-depdom)
 
 # wxPython
 set(WXPYTHON_URL
-    http://cfhcable.dl.sourceforge.net/project/wxpython/wxPython/2.8.12.1/wxPython-src-2.8.12.1.tar.bz2)
+    ${CMAKE_SOURCE_DIR}/build/3rdparty-dists/wxPython-src-2.8.12.1.tar.bz2)
+    #http://cfhcable.dl.sourceforge.net/project/wxpython/wxPython/2.8.12.1/wxPython-src-2.8.12.1.tar.bz2)
 set(WXPYTHON_DIR ${CMAKE_CURRENT_BINARY_DIR}/wxpython)
 set(WXPYTHON_INSTALL ${THIRD_PARTY_INSTALL_DIR}/wxpython)
+set(WX_INSTALL ${THIRD_PARTY_INSTALL_DIR}/wxwidgets)
 set(WXPYTHON_SRC ${WXPYTHON_DIR}/source)
 
 ExternalProject_Add(wxpython
     URL ${WXPYTHON_URL}
     PREFIX ${WXPYTHON_DIR}
     SOURCE_DIR ${WXPYTHON_SRC}
-    INSTALL_DIR ${WXPYTHON_INSTALL}
+    INSTALL_DIR ${WX_INSTALL}
     CONFIGURE_COMMAND
-        sh ${WXPYTHON_SRC}/configure --prefix=${WXPYTHON_INSTALL} --with-gtk --with-gnomeprint --with-opengl --enable-geometry --enable-graphics_ctx --enable-sound --with-sdl --enable-mediactrl --enable-display
+        sh ${WXPYTHON_SRC}/configure --prefix=${WX_INSTALL} --with-gtk --with-gnomeprint --with-opengl --enable-geometry --enable-graphics_ctx --enable-sound --with-sdl --enable-mediactrl --enable-display
     BUILD_COMMAND
         make &&
         make -C contrib/src/gizmos &&
@@ -64,14 +72,15 @@ ExternalProject_Add(wxpython
         python setup.py build_ext --inplace UNICODE=0 BUILD_GLCANVAS=0 BUILD_OGL=0 &&
         python setup.py install UNICODE=0 --home=${WXPYTHON_INSTALL}
 )
-set(WX_INCLUDE ${WXPYTHON_INSTALL}/include/wx-2.8 ${WXPYTHON_INSTALL}/lib/wx/include/gtk2-ansi-release-2.8)
-set(WX_LIB_DIR ${WXPYTHON_INSTALL}/lib)
+set(WX_INCLUDE ${WX_INSTALL}/include/wx-2.8 ${WX_INSTALL}/lib/wx/include/gtk2-ansi-release-2.8)
+set(WX_LIB_DIR ${WX_INSTALL}/lib)
 set(WX_FLAGS -D_FILE_OFFSET_BITS=64 -D_LARGE_FILES -D__WXGTK__ -pthread)
-set(WX_LIBRARIES ewxaui -lwx_gtk2_adv-2.8 -lwx_gtk2_richtext-2.8 -lwx_gtk2_aui-2.8 -lwx_gtk2_xrc-2.8 -lwx_gtk2_qa-2.8 -lwx_gtk2_html-2.8 -lwx_gtk2_adv-2.8 -lwx_gtk2_core-2.8 -lwx_base_xml-2.8 -lwx_base_net-2.8 -lwx_base-2.8)
+set(WX_LIBRARIES -lwx_gtk2_adv-2.8 -lwx_gtk2_richtext-2.8 -lwx_gtk2_aui-2.8 -lwx_gtk2_xrc-2.8 -lwx_gtk2_qa-2.8 -lwx_gtk2_html-2.8  -lwx_gtk2_core-2.8 -lwx_base_xml-2.8 -lwx_base_net-2.8 -lwx_base-2.8 ewxaui)
 
 # HTTPD
 set(HTTPD_URL
-    http://apache.mirrors.pair.com//httpd/httpd-2.2.34.tar.gz)
+    ${CMAKE_SOURCE_DIR}/build/3rdparty-dists/httpd-2.2.25.tar.bz2)
+    #http://apache.mirrors.pair.com//httpd/httpd-2.2.34.tar.gz)
 set(HTTPD_DIR ${CMAKE_CURRENT_BINARY_DIR}/httpd)
 set(HTTPD_INSTALL ${THIRD_PARTY_INSTALL_DIR}/httpd)
 set(HTTPD_SRC ${HTTPD_DIR}/source)
@@ -93,7 +102,8 @@ ExternalProject_Add(httpd
 #set(ACTIVEMQ_URL
 #    https://github.com/FriendsofECCE/ECCE/raw/develop/build/3rdparty-dists/apache-activemq-5.1.0-bin.tar.bz2)
 set(ACTIVEMQ_URL
-    http://archive.apache.org/dist/activemq/apache-activemq/5.1.0/apache-activemq-5.1.0-bin.tar.gz)
+    ${CMAKE_SOURCE_DIR}/build/3rdparty-dists/apache-activemq-5.1.0-bin.tar.bz2)
+    #http://archive.apache.org/dist/activemq/apache-activemq/5.1.0/apache-activemq-5.1.0-bin.tar.gz)
 set(ACTIVEMQ_DIR ${CMAKE_CURRENT_BINARY_DIR}/activemq)
 set(ACTIVEMQ_INSTALL ${THIRD_PARTY_INSTALL_DIR}/activemq)
 set(ACTIVEMQ_SRC ${ACTIVEMQ_DIR}/source)
